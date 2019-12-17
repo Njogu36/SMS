29786871c141c9a8c690e9398906e4e481ea816d
Evans Mburu Njogu <39052232+Njogu36@users.noreply.github.com>
2019-12-18 01:01:45 +0300

    Update key.js

--- config/key.js
+++ config/key.js
@@ -1,7 +1,7 @@
 module.exports = {
   
-    database:'mongodb://admin:Becalmevans36@ds241288.mlab.com:41288/elite-sms',
-    //database:'mongodb://localhost/elite-sms',
+   // database:'mongodb://admin:Becalmevans36@ds241288.mlab.com:41288/elite-sms',
+    database:'mongodb://localhost/elite-sms',
     apiKey:'',
     username:'',
     secret: 'admin'
